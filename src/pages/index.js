a74c64dff88ab3bc61381895890f4bba45acf0de
Luke <lukejoliat@gmail.com>
2019-03-02 01:23:28 -0500

    update svg path

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import SvgWithXlink from '../components/Svg'
-import Mac from '../images/mac.svg'
+import Mac from '../images/Mac.svg'
 const IndexPage = ({ data }) => (
   <Layout>
     <section className="container">
